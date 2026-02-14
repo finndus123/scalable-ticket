c5a39bd5d6b9a0f1cf02207a2baad37998791562
finndus123 <f.sudmann123@googlemail.com>
2026-02-14 17:57:22 +0100

    refactored TicketPurchaseRequest to Record instead of class

--- backend/ticket-common/src/main/java/de/playground/scalable_ticketing/common/dto/TicketPurchaseRequest.java
+++ backend/ticket-common/src/main/java/de/playground/scalable_ticketing/common/dto/TicketPurchaseRequest.java
@@ -1,77 +1,13 @@
 package de.playground.scalable_ticketing.common.dto;
 
-import java.io.Serializable;
-
 /**
- * Class representing a ticket purchase request event.
+ * Record representing a ticket purchase request event.
  * Is used as a shared data model between services accessing the queue.
  */
-public class TicketPurchaseRequest implements Serializable {
-
-    private String requestId;
-    private String eventId;
-    private String userId;
-    private int quantity;
-    private String timestamp;
-
-    public TicketPurchaseRequest() {}
-
-    public TicketPurchaseRequest(String requestId, String eventId, String userId, int quantity, String timestamp) {
-        this.requestId = requestId;
-        this.eventId = eventId;
-        this.userId = userId;
-        this.quantity = quantity;
-        this.timestamp = timestamp;
-    }
-
-    public String getRequestId() {
-        return requestId;
-    }
-
-    public void setRequestId(String requestId) {
-        this.requestId = requestId;
-    }
-
-    public String getEventId() {
-        return eventId;
-    }
-
-    public void setEventId(String eventId) {
-        this.eventId = eventId;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    @Override
-    public String toString() {
-        return "TicketPurchaseRequest{" +
-                "requestId='" + requestId + '\'' +
-                ", eventId='" + eventId + '\'' +
-                ", userId='" + userId + '\'' +
-                ", quantity=" + quantity +
-                ", timestamp='" + timestamp + '\'' +
-                '}';
-    }
-}
+public record TicketPurchaseRequest(
+    String requestId,
+    String eventId,
+    String userId,
+    int quantity,
+    String timestamp
+) {}
